65806b9d70b84e1783c6ed8d7db3a9fdfea982af
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-05-21 19:23:23 +0530

    feat: Add comparison operations and NaN check in conversionOperation.js

--- 01-basics/conversionOperation.js
+++ 01-basics/conversionOperation.js
@@ -40,7 +40,18 @@
 // let str3 = str1 + str2;
 // console.log(str3)
 
-console.log(1 + "2");
-console.log("1" + 2);
-console.log("1" + 2 + 2);
-console.log(1 + 2 + 2);  
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + 2);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null === 0);
+// console.log(null < 0);
+// console.log(null > 0);
+
+console.log(NaN === NaN);
