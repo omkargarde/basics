dd000142857a6276d07a6980b32aca096971328b
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-05-21 16:47:06 +0530

    chore: update variable.js and dataTypes.js files
    Add proper spacing in console.table in variable.js.
    Add explanations and examples of primitive datatypes in JavaScript in dataTypes.js.

--- 01-basics/variable.js
+++ 01-basics/variable.js
@@ -7,4 +7,6 @@ let accountState;
 
 console.log(accountId);
 
-console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+// MARK: prefer not to use var because of issue in block scope and functional scope
