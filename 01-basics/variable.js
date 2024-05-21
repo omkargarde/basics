6a36aec66003dcde214af22513a4c1b3fdb6b355
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-05-21 12:32:25 +0530

    init

---
+++ 01-basics/variable.js
@@ -0,0 +1,10 @@
+const accountId = 12243;
+let accountEmail = "test@test.com";
+var accountPassword = "12341";
+accountCity = "belgaum";
+
+// accountId = 2;
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
