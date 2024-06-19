31e7752e008d90716388179f2b5f0cf8f516d460
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-06-19 16:25:09 +0530

    update

---
+++ 01-basics/string.js
@@ -0,0 +1,7 @@
+const name = "omkar";
+const count = 50;
+console.log(name + count);
+
+// string interpolation is modern way of working with String
+// string function can be called in it
+console.log(`my name is ${name.toUpperCase()}`);
